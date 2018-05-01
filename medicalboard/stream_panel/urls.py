8a78893cc842236e63cf10230a66e6ee0580bd9d
japrogramer <japrogramer@yahoo.com>
2018-05-01 17:07:52 -0500

    Fix reverse for comments.

--- medicalboard/stream_panel/urls.py
+++ medicalboard/stream_panel/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, re_path, path
 
 from . import views
 
-app_name = "panels"
+app_name = "stream_panel"
 urlpatterns = [
     re_path('^add/comments/(?P<panel_id>\d+)', login_required(views.CommentsView.as_view()), name='add_comment'),
 ]
